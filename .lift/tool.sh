96c55f2f343a8437284edc9f7b97d5f709cc5f15
agustin-mrtz <108492518+agustin-mrtz@users.noreply.github.com>
2022-08-19 13:55:10 -0700

    Update tool.sh

--- .lift/tool.sh
+++ .lift/tool.sh
@@ -1,14 +1,9 @@
 #!/usr/bin/env bash
 function run() {
-    for i in $(git ls-files | grep -v lift) ; do
-        if ! jq $i >/dev/null 2>/dev/null ; then
-            echo "[ { \"type\" : \"bad json\", \"file\" : \"\$i\", \"line\": 1,\
-                      \"message\" : \"JSON is many things, but it is not this.\",\
-                      \"details_url\": null } ]"
-            exit 0
-        fi
-    done
-    echo "[ ]" ; exit 0
+    echo "[ { \"type\" : \"bad json\", \"file\" : \"$PWD/tool.sh\", \"line\": 1,\
+              \"message\" : \"JSON is many things, but it is not this.\",\
+              \"details_url\": null } ]"
+    exit 0
 }
 
 [[ "$3" = "version" ]] && echo "1"
